36916f414d9f3618a5565799ad5ca82e49df9928
Kevin Wang <wy721@qq.com>
2024-01-28 21:32:11 +0800

    Auto select ipv4 or ipv6

--- src/client.rs
+++ src/client.rs
@@ -11,7 +11,7 @@ use log::debug;
 use crate::ntp::client::{run_nts_ntp_client, NtpResult};
 use crate::nts_ke::client::{run_nts_ke_client, ClientConfig};
 
-pub async fn get_time(host: &str, port: Option<u16>, use_ipv6: bool) -> Result<NtpResult> {
+async fn get_time_ip_ver(host: &str, port: Option<u16>, use_ipv6: bool) -> Result<NtpResult> {
     let config = ClientConfig {
         host: host.into(),
         port,
@@ -26,8 +26,21 @@ pub async fn get_time(host: &str, port: Option<u16>, use_ipv6: bool) -> Result<N
         .context("failed to get time")
 }
 
+/// get_time gets the time from given NTS server.
+pub async fn get_time(host: &str, port: Option<u16>) -> Result<NtpResult> {
+    let v4_result = get_time_ip_ver(host, port, false).await;
+    if v4_result.is_ok() {
+        return v4_result;
+    }
+    let v6_result = get_time_ip_ver(host, port, true).await;
+    if v6_result.is_ok() {
+        return v6_result;
+    }
+    v4_result
+}
+
 #[tokio::test]
 async fn it_works() {
-    let result = get_time("time.cloudflare.com", None, false).await.unwrap();
+    let result = get_time("time.cloudflare.com", None).await.unwrap();
     assert!(result.time_diff < 10.);
 }
